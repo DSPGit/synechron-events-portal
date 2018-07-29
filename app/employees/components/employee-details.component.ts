729b5ba9406dacef691294fb7633fbc89236af55
dattaprasad patil <admin@TRNROOM5-08.synechron.com>
2018-07-29 17:48:09 +0530

    Day4: fetched employee based on id and display its details

--- app/employees/components/employee-details.component.ts
+++ app/employees/components/employee-details.component.ts
@@ -1,14 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Inject, OnInit } from '@angular/core';
 
+//services
+import { SepEmployeesService } from "../services/sep-employees.service";
+import { ActivatedRoute } from '@angular/router';
+//models
 import { Employee } from "../models/employee";
+import { SepEmployee } from '../models/sep-employee';
 
 @Component({
     selector: 'employee-details',
     templateUrl: '../views/employee-details.component.html'
 })
 
-export class EmployeeDetailsComponent {
-    constructor() { }
-    title: string = "Details of - ";
-    @Input() employee: Employee;
+export class EmployeeDetailsComponent implements OnInit {
+
+    employeeId: number;
+    employee: SepEmployee;
+    title: string = "Details of Employee - ";
+
+    constructor(
+        @Inject(SepEmployeesService) private _employeesService: SepEmployeesService,
+        @Inject(ActivatedRoute) private _route: ActivatedRoute
+    ) { }
+
+    ngOnInit(): void {
+        this._route.params.subscribe((params) => this.employeeId = params["id"])
+        this._employeesService.getSingleEmployee(this.employeeId).subscribe(
+            (data) => this.employee = data,
+            (err) => console.error(err),
+            () => console.log("Fetch one employee service complete!!")
+        )
+        console.log(this.employee);
+    }
 }
