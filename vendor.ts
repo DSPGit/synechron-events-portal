3058d182743268962909dc4b5ccecbab61c6a14b
dattaprasad patil <admin@TRNROOM5-08.synechron.com>
2018-07-29 11:20:57 +0530

    Day 4: installed Router module and included in vendor.ts

--- vendor.ts
+++ vendor.ts
@@ -3,7 +3,8 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-import '@angular/forms'
+import '@angular/forms';
+import '@angular/router';
 
 import 'rxjs';
 
